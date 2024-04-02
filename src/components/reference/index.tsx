b00f41998f4c7f9266dfc6d4c0363bd1f335919f
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-02 19:37:33 +0300

    refactor: adds reference component to create standarts

---
+++ src/components/reference/index.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import styles from "./reference.module.scss";
+
+/*
+  Maintain consistency between interface and component naming:
+  For instance, use 'ReferenceProps' for the interface and 'Reference' for the component.
+*/
+
+export interface ReferenceProps {
+  name: string;
+}
+
+/*
+  When creating a new component, follow this template:
+  export default function componentName(props: ComponentProps) {
+     Component content goes here
+  }
+*/
+export default function Reference(props: ReferenceProps) {
+  return (
+    /*
+      Adhere to BEM (Block Element Modifier) naming conventions for classnames.
+
+      Example of BEM naming convention:
+      - Block: reference
+      - Element: description, top
+      - Modifier: --large, --active
+      */
+    <div className={styles["reference"]}>
+      <div className={styles["reference_description"]}></div>
+      <div className={styles["reference--top"]}></div>
+    </div>
+  );
+}
---
+++ src/components/reference/reference.test.ts
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+// Import the component to be tested and its props
+import Reference, { ReferenceProps } from "./index";
+
+// Describe the test suite for the Reference component
+describe("Reference component", () => {
+  // Define mock props for the component
+  const props: ReferenceProps = {
+    name: "Test Name",
+  };
+
+  // Test case: Ensure that the component renders without crashing
+  it("renders without crashing", () => {
+    // Render the Reference component with mock props
+    render(Reference(props));
+  });
+
+  // You can add more test cases here as needed
+});
