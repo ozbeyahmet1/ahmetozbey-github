bec4fcc6178ca4b8415af954773dbbee21af4f95
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-05 16:36:15 +0300

    feat: creates pagination component

---
+++ src/components/ui/pagination/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styles from './pagination.module.scss';
+import Input from '../input';
+
+export interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+export default function Pagination({ currentPage, totalPages }: PaginationProps) {
+  const [currentPageNumber, setCurrentPageNumber] = React.useState<number>(currentPage);
+  return (
+    <div className={styles['pagination']}>
+      <Input
+        type="number"
+        value={currentPageNumber}
+        onChange={(e) => setCurrentPageNumber(parseInt(e.target.value))}
+        error={false}
+        name="page"
+        placeholder={currentPage.toString()}
+        className={styles['pagination_input']}
+        min={1}
+      />
+      /
+      <p>{totalPages}</p>
+    </div>
+  );
+}
