b278cba4d812556afdc4957e569acf579e718cd0
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-07 22:01:11 +0300

    refactor: refactors styling of header

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -5,7 +5,7 @@ import styles from "./header.module.scss";
 export default function Header() {
   return (
     <header className={styles["header"]}>
-      <div className={styles["header_inner"]}>
+      <div className={styles["header_inner"] + " container"}>
         <Logo />
         <div className={styles["header--right"]}>
           <BackgroundDial />
