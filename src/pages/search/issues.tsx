7c74b8e79dc56eb0e8c8eba68aa3f59510662728
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-08 21:51:35 +0300

    feat: creates search page

---
+++ src/pages/search/issues.tsx
@@ -0,0 +1,24 @@
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { SearchPageProps, SearchPageResult } from "@/helpers/interfaces/pageProps";
+import { fetchSearchPageResultsByQuery } from "@/helpers/utils/apiFunctions";
+import Template from "@/layouts/template";
+import SearchResultsPageView from "@/views/searchResults";
+
+export default function Home({ searchPageResult }: SearchPageProps) {
+  console.log("arr", searchPageResult);
+  return (
+    <Template>
+      <SearchResultsPageView data={searchPageResult.items} />
+    </Template>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<SearchPageProps> = async ({ query }) => {
+  const { q } = query;
+  const data = await fetchSearchPageResultsByQuery(q as string);
+  const homepageData: SearchPageProps = { searchPageResult: data as SearchPageResult };
+  return {
+    props: homepageData,
+  };
+};
