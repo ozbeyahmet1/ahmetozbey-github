4e9187d3c6fc4f07f455ccfd0e425021f4cd0c41
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-08 23:04:31 +0300

    feat: adds animation to table

--- src/views/issues/index.tsx
+++ src/views/issues/index.tsx
@@ -1,8 +1,8 @@
+import { AnimatePresence, AnimationProps, motion } from "framer-motion";
 import { useState } from "react";
 import Table from "@/components/table/index";
 import Button from "@/components/ui/button";
 import Input from "@/components/ui/input";
-import Pagination from "@/components/ui/pagination";
 import { Issue } from "@/helpers/interfaces/api";
 import { IssuePageProps } from "@/helpers/interfaces/pageProps";
 import styles from "./issuesView.module.scss";
@@ -21,20 +21,22 @@ export default function IssuePageView({ data }: IssuePageProps) {
   return (
     <div className={styles["issuePageView"]}>
       <div className={styles["issuePageView_container"]}>
-        <div className={styles["issuePageView--top"]}>
-          <Input
-            className={styles["issuePageView_input"]}
-            value={search}
-            type="text"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
-            name="search"
-            placeholder="Search by Repository, Project and Contributors"
-          />
-          <Button className={styles["issuePageView_searchButton"]}>
-            <p>Search</p>
-          </Button>
-        </div>
-        <Table data={search ? updatedData : data} />
+        <motion.div initial={{ opacity: 0, y: -200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+          <div className={styles["issuePageView--top"]}>
+            <Input
+              className={styles["issuePageView_input"]}
+              value={search}
+              type="text"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
+              name="search"
+              placeholder="Search by Repository, Project and Contributors"
+            />
+            <Button className={styles["issuePageView_searchButton"]}>
+              <p>Search</p>
+            </Button>
+          </div>
+          <Table data={search ? updatedData : data} />
+        </motion.div>
       </div>
     </div>
   );
